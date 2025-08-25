c447e225fb144afddf42bdf2e2ea422ec228dc51
Narayan7218 <narayanawate92@gmail.com>
2025-08-25 04:12:18 +0000

    conversionof datatype

---
+++ .devcontainer/01_basics/03_coonversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let  valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" =>33
+// "33abc" => 1; false => 0
+//true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(booleanInLoggedIn);
+
+// 1 => true; 0 =>false
+// "" =>false
+// "Narayan" =>true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
